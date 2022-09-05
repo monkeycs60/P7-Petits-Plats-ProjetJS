fe670c724ae9091ca0551744c58547e2c9045a8f
monkeycs60 <clement.serizay@gmail.com>
2022-09-05 02:14:26 +0200

    finition cards css

--- scripts/utils/recipeCardFactory.js
+++ scripts/utils/recipeCardFactory.js
@@ -20,17 +20,17 @@ export function recipeCardFactory(recipe) {
     <img class="w-[25vw] h-[17vh] rounded-lg rounded-b-none object-cover" src="./../../assets/restaurantpreview.png" alt="restaurant card" />
     </div>
     <div id="article-content" class="w-[25vw] h-[18vh] p-5 flex flex-col justify-center bg-orange-600 rounded-lg rounded-t-none">
-    <div id="card-top" class="h-[5vh] flex w-full justify-between items-center bg-blue-700">
-        <h2 class="text-xl">${name}</h2>
-        <div id="clock-time" class="flex w-1/5 justify-around items-center text-xl bg-yellow-400" >
+    <div id="card-top" class="h-[6vh] flex w-full justify-between items-center bg-blue-700">
+        <h2 class="text-xl block-with-text">${name}</h2>
+        <div id="clock-time" class="flex w-1/6 justify-around items-center text-xl bg-yellow-400" >
             <em class="fa-regular fa-clock"></em>
             <p class="font-bold">${time}</p>
         </div>
     </div>
-    <div id="card-bottom" class="flex h-[13vw]">
-        <ul class="ingredientsList text-xs w-1/2 bg-red-300">
+    <div id="card-bottom" class="h-[12vw] flex pt-[1vh]">
+        <ul class="ingredientsList text-sm w-1/2 bg-red-300 overflow-spots-fullrecipe">
         </ul>
-        <p class="description text-xs w-1/2 overflow-spots">${description}</p>
+        <p class="description text-sm w-1/2 overflow-spots-fullrecipe">${description}</p>
     </div>
 
     </div>
@@ -39,12 +39,12 @@ export function recipeCardFactory(recipe) {
     ingredients.forEach((uniqueIngredient) => {
       const { ingredient, quantity, unit } = uniqueIngredient;
       const li = document.createElement("li");
-      li.textContent = `${ingredient}: ${quantity} ${unit} `;
+      li.innerHTML = `<span class="font-bold">${ingredient}</span>: ${quantity} ${unit} `;
       if (quantity === undefined) {
-        li.innerText = `${ingredient}: ${unit} `;
+        li.innerHTML = `<span class="font-bold">${ingredient}</span>: ${unit} `;
       }
       if (unit === undefined) {
-        li.innerText = `${ingredient}: ${quantity} `;
+        li.innerHTML = `<span class="font-bold">${ingredient}</span>: ${quantity}`;
       }
       listIngredients.appendChild(li);
     });
