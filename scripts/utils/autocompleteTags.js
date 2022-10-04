2bd184173005bc2b63adb922345584cbcf338696
monkeycs60 <clement.serizay@gmail.com>
2022-10-04 18:07:35 +0200

    fin pour aujourd'hui

--- scripts/utils/autocompleteTags.js
+++ scripts/utils/autocompleteTags.js
@@ -7,56 +7,141 @@ const inputAppliances = document.querySelector("#inputAppliances");
 const inputUstensils = document.querySelector("#inputUstensils");
 
 inputIngredients.addEventListener("keyup", (e) => {
-  
   const inputValue = e.target.value.toLocaleLowerCase();
-  console.log(inputValue);
-  let filteredIngredients = [];
   const ingredientsTagsList = document.querySelectorAll(".ingredientsTagsList");
-  // for each ingredientTagList that are not display none, push it in filteredIngredients
-  ingredientsTagsList.forEach((ing) => {
-    if (ing.style.display !== "none") {
-      filteredIngredients.push(ing.textContent.toLocaleLowerCase());
-    }
-  });
-  console.log(filteredIngredients);
-  // élimine les valeurs qui ne commencent pas par la valeur de l'input
-  const filteredIngredients2 = filteredIngredients.filter((ing) => {
-    return ing.includes(inputValue);
-  });
- 
-  // on affiche les tags qui correspondent à la valeur de l'input
-  ingredientsTagsList.forEach((ing) => {
-    if (!filteredIngredients2.includes(ing.textContent.toLocaleLowerCase())) {
-        ing.style.display = "none";
+
+  let ingredientsTagsListArray = [];
+
+  const allArticles = document.querySelectorAll("article");
+  const allArticlesArray = Array.from(allArticles);
+  const filteredArticles = [];
+
+  allArticlesArray.forEach((article) => {
+    if (article.style.display === "none") {
+    } else {
+      filteredArticles.push(article);
+      // push its preciseIngredients in ingredientsTagsListArray
+      article.querySelectorAll(".preciseIngredient").forEach((ing) => {
+        ingredientsTagsListArray.push(ing.textContent);
+      });
     }
   });
 
-  
+  ingredientsTagsListArray = [...new Set(ingredientsTagsListArray)].sort();
+  console.log(ingredientsTagsListArray);
+
+  // mettre tous les mots de filteredIngredientsTagsListArray en minuscule
+  ingredientsTagsListArray = ingredientsTagsListArray.map((ing) =>
+    ing.toLocaleLowerCase()
+  );
+
+  // vider le tableau des valeurs non include dans l'input
+  const filteredIngredientsTagsListArray = ingredientsTagsListArray.filter(
+    (ing) => ing.includes(inputValue.toLocaleLowerCase())
+  );
+
+  // for each ingredientsTagsList, if it is not in filteredIngredientsTagsListArray, we hide it
+  ingredientsTagsList.forEach((ing) => {
+    if (
+      !filteredIngredientsTagsListArray.includes(
+        ing.textContent.toLocaleLowerCase()
+      )
+    ) {
+      ing.style.display = "none";
+    } else {
+      ing.style.display = "block";
+    }
+  });
 });
 
 inputAppliances.addEventListener("keyup", (e) => {
-    const inputValue = e.target.value;
-    const appliancesTagsList = document.querySelectorAll(".appliancesTagsList");
-    
-    appliancesTagsList.forEach((tag) => {
-        if (tag.innerText.toLowerCase().includes(inputValue.toLowerCase())) {
-        tag.style.display = "block";
-        } else {
-        tag.style.display = "none";
-        }
-    });
+  const inputValue = e.target.value;
+  const appliancesTagsList = document.querySelectorAll(".appliancesTagsList");
+  let filteredAppliancesArray = [];
+
+  const allArticles = document.querySelectorAll("article");
+  const allArticlesArray = Array.from(allArticles);
+  const filteredArticles = [];
+
+  allArticlesArray.forEach((article) => {
+    if (article.style.display === "none") {
+    } else {
+      filteredArticles.push(article);
+      // push its preciseIngredients in ingredientsTagsListArray
+      article.querySelectorAll(".applianceTag").forEach((ing) => {
+        filteredAppliancesArray.push(ing.textContent);
+      });
+    }
+  });
+
+  filteredAppliancesArray = [...new Set(filteredAppliancesArray)].sort();
+  
+
+  // mettre tous les mots de filteredAppliancesArray en minuscule
+   filteredAppliancesArray = filteredAppliancesArray.map((appliance) => appliance.toLocaleLowerCase());
+  console.log(filteredAppliancesArray);
+
+  console.log(inputValue.toLocaleLowerCase());
+  // vider le tableau des valeurs non include dans l'input
+  const filteredAppliancesTagsListArray = filteredAppliancesArray.filter(
+    (ing) => ing.includes(inputValue.toLocaleLowerCase())
+  );
+
+
+  // for each ingredientsTagsList, if it is not in filteredIngredientsTagsListArray, we hide it
+  appliancesTagsList.forEach((ing) => {
+    if (!filteredAppliancesTagsListArray.includes(ing.textContent.toLocaleLowerCase())) {
+      ing.style.display = "none";
+    } else {
+      ing.style.display = "block";
+    }
+  });
 });
 
 inputUstensils.addEventListener("keyup", (e) => {
     const inputValue = e.target.value;
     const ustensilsTagsList = document.querySelectorAll(".ustensilsTagsList");
     
-    ustensilsTagsList.forEach((tag) => {
-        if (tag.innerText.toLowerCase().includes(inputValue.toLowerCase())) {
-        tag.style.display = "block";
+    let filteredUstensils = [];
+    
+     const allArticles = document.querySelectorAll("article");
+     const allArticlesArray = Array.from(allArticles);
+     const filteredArticles = [];
+
+     allArticlesArray.forEach((article) => {
+       if (article.style.display === "none") {
+       } else {
+         filteredArticles.push(article);
+         // push its preciseIngredients in ingredientsTagsListArray
+         article.querySelectorAll(".ustensilTag").forEach((ing) => {
+           filteredUstensils.push(ing.textContent);
+         });
+       }
+     });
+
+      filteredUstensils = [...new Set(filteredUstensils)].sort();
+      console.log(filteredUstensils);
+
+      // mettre tous les mots de filteredUstensils en minuscule
+      filteredUstensils = filteredUstensils.map((ustensil) => ustensil.toLocaleLowerCase());
+      console.log(filteredUstensils);
+
+      console.log(inputValue.toLocaleLowerCase());
+      // vider le tableau des valeurs non include dans l'input
+      const filteredUstensilsTagsListArray = filteredUstensils.filter(
+        (ing) => ing.includes(inputValue.toLocaleLowerCase())
+      );
+
+      // for each ingredientsTagsList, if it is not in filteredIngredientsTagsListArray, we hide it
+      ustensilsTagsList.forEach((ing) => {
+        if (!filteredUstensilsTagsListArray.includes(ing.textContent.toLocaleLowerCase())) {
+          ing.style.display = "none";
         } else {
-        tag.style.display = "none";
+          ing.style.display = "block";
         }
-    });
+      });
+      
+
+
 });
 }
