b4b3f785d6b7149028823470c55434d8c09e797e
monkeycs60 <clement.serizay@gmail.com>
2022-10-07 13:10:11 +0200

    cross actualisation tags

--- scripts/utils/killTags.js
+++ scripts/utils/killTags.js
@@ -12,49 +12,151 @@ export function killTags(
   console.log(killCross);
   killCross.forEach((cross) => {
     cross.addEventListener("click", () => {
-      console.log("cross listener");
-
-      console.log(ingredientsTags);
-      console.log(appliancesTags);
+      ingredientsTagsActualized = [];
+      appliancesTagsActualized = [];
+      ustensilsTagsActualized = [];
 
       console.log(tagsArrayFilter);
 
-        // on supprime le tag du tableau des tags sélectionnés  
-        tagsArrayFilter.splice(tagsArrayFilter.indexOf(cross.previousSibling.textContent), 1);
-        console.log(tagsArrayFilter);
+      // on supprime le tag du tableau des tags sélectionnés
+      tagsArrayFilter.splice(
+        tagsArrayFilter.indexOf(cross.previousSibling.textContent),
+        1
+      );
+      console.log(tagsArrayFilter);
 
-        // on supprime le tag du DOM
-        cross.parentNode.remove();
+      // on supprime le tag du DOM
+      cross.parentNode.remove();
 
-        const allArticles = Array.from(document.querySelectorAll("article"));
-        
-       allArticles.forEach((article) => {
+      const allArticles = Array.from(document.querySelectorAll("article"));
+
+      allArticles.forEach((article) => {
         // les 3 champs de la recherche avancée
-        let allIngredients = [];
-        const articleIngredients = Array.from(article.querySelectorAll(".preciseIngredient"));
+        const allIngredients = [];
+        const articleIngredients = Array.from(
+          article.querySelectorAll(".preciseIngredient")
+        );
         articleIngredients.forEach((ingredient) => {
-            allIngredients.push(ingredient.textContent.toLocaleLowerCase());
+          allIngredients.push(ingredient.textContent.toLocaleLowerCase());
         });
-        const articleAppliance = article.querySelector(".applianceTag").textContent;
-        let allUstensils = [];
-        const articleUstensils = Array.from(article.querySelectorAll(".ustensilTag"));
+        const articleAppliance =
+          article.querySelector(".applianceTag").textContent;
+        const allUstensils = [];
+        const articleUstensils = Array.from(
+          article.querySelectorAll(".ustensilTag")
+        );
         articleUstensils.forEach((ustensil) => {
-            allUstensils.push(ustensil.textContent.toLocaleLowerCase());
+          allUstensils.push(ustensil.textContent.toLocaleLowerCase());
         });
 
         // le reste de l'article (description & titre)
-        const articleDescription = article.querySelector(".recipe-title").textContent;
+        const articleDescription =
+          article.querySelector(".recipe-title").textContent;
         const articleTitle = article.querySelector(".description").textContent;
+        const articleIndividualIngredients = Array.from(
+          article.querySelectorAll(".preciseIngredient")
+        );
+        const articleUstensilTag = Array.from(
+          article.querySelectorAll(".ustensilTag")
+        );
+        const articleApplianceTag = article.querySelector(".applianceTag");
 
         // il faut montrer les articles qui correspondent à l'input de 3 lettres, puis filtrer
-        const inputValue = document.getElementById("mainSearch");
+        const inputValue = document.getElementById("mainSearch").value;
         console.log(inputValue);
-        console.log(inputValue.value);
 
-    })
-    
+        // Le cas où l'INPUT est déjà rempli
+        if (inputValue.length > 2) {
+          // on affiche les articles qui correspondent à l'input
+          if (
+            articleDescription.includes(inputValue) ||
+            articleTitle.includes(inputValue) ||
+            allIngredients.includes(inputValue)
+          ) {
+            article.style.display = "flex";
+            console.log("input > 2 ET article affiché");
+            // on push articleIndividualIngredients dans le tableau actualisé
+            articleIndividualIngredients.forEach((ingredient) => {
+              ingredientsTagsActualized.push(ingredient.textContent);
+            });
+            // on push articleUstensilTag dans le tableau actualisé
+            articleUstensilTag.forEach((ustensil) => {
+              ustensilsTagsActualized.push(ustensil.textContent);
+            });
+            // on push articleApplianceTag dans le tableau actualisé
+            appliancesTagsActualized.push(articleApplianceTag.textContent);
+          } else {
+            console.log("input > 2 mais ne correspond pas");
+            article.style.display = "none";
+          }
+        } else {
+          console.log("input COURT < 2");
+          // on affiche tous les articles
+          article.style.display = "flex";
+        }
+      });
+      // on supprime les doublons et on trie les tableaux
+      ingredientsTagsActualized = [
+        ...new Set(ingredientsTagsActualized),
+      ].sort();
+      appliancesTagsActualized = [...new Set(appliancesTagsActualized)].sort();
+      ustensilsTagsActualized = [...new Set(ustensilsTagsActualized)].sort(
+        (a, b) => a.localeCompare(b)
+      );
 
-     
+      ingredientsTagsActualized.forEach((ingredient) => {
+        ingredient.toLowerCase();
+      });
+      appliancesTagsActualized.forEach((appliance) => {
+        appliance.toLowerCase();
+      });
+      ustensilsTagsActualized.forEach((ustensil) => {
+        ustensil.toLowerCase();
+      });
+
+      
+      ingredientsTagsActualized = ingredientsTagsActualized.map((ingredient) => {
+        return ingredient.charAt(0).toUpperCase() + ingredient.slice(1);
+      });
+      ustensilsTagsActualized = ustensilsTagsActualized.map((ustensil) => {
+        return ustensil.charAt(0).toUpperCase() + ustensil.slice(1);
+      });
+      //same for appliance
+      appliancesTagsActualized = appliancesTagsActualized.map((appliance) => {
+        return appliance.charAt(0).toUpperCase() + appliance.slice(1);
+      });
+      
+
+      const ingredientsTags = Array.from(
+        document.querySelectorAll(".ingredientsTagsList")
+      );
+      const appliancesTags = Array.from(document.querySelectorAll(".appliancesTagsList"));
+      const ustensilsTags = Array.from(document.querySelectorAll(".ustensilsTagsList"));
+
+      // on ne display pas les tags qui ne correspondent pas à la recherche
+      ingredientsTags.forEach((ingredient) => {
+        if (ingredientsTagsActualized.includes(ingredient.textContent)) {
+          ingredient.style.display = "block";
+        } else {
+          ingredient.style.display = "none";
+        }
+      });
+      appliancesTags.forEach((appliance) => {
+        console.log(appliance);
+        if (appliancesTagsActualized.includes(appliance.textContent)) {
+          appliance.style.display = "block";
+        } else {
+          appliance.style.display = "none";
+        }
+      });
+      ustensilsTags.forEach((ustensil) => {
+        
+        if (ustensilsTagsActualized.includes(ustensil.textContent)) {
+          ustensil.style.display = "block";
+        } else {
+          ustensil.style.display = "none";
+        }
+      });
     });
   });
 }
